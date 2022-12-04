7a08afccc1419463c9303a0fb1ce5f4e8b05b19f
Jackson <alexisjacksonlon213@gmail.com>
2022-12-04 14:41:58 -0500

    Landing Page - Travely

---
+++ src/Components/UI/NavHome/NavHome.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Ancla } from '../Ancla/Ancla';
+
+export const NavHome = () => {
+  return (
+    <nav className='navHome'>
+      <Ancla 
+        style="ancla" 
+        reference="#" 
+        textAncla="Home"
+      />
+       <Ancla 
+        style="ancla" 
+        reference="#" 
+        textAncla="Destination"
+      />
+       <Ancla 
+        style="ancla" 
+        reference="#" 
+        textAncla="Expedition"
+      />
+        <Ancla 
+        style="ancla" 
+        reference="#" 
+        textAncla="FAQ"
+      />
+    </nav>
+  )
+}
